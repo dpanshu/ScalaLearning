c4f2ddcc85b2dabd133b6704db78dadec724a6f5
dpanshu <deepanshu.iitk@gmail.com>
2016-08-04 06:35:27 +0530

    Extra addition

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -10,10 +10,10 @@ val s = "Hello World"
 println(s)
 
 // get the length of the string
-lengthString = s.length
+var lengthString = s.length
 
 // concatenate 2 strings
-newString = "Hello" + "World"
+var newString = "Hello" + " " + "World"
 
 // iterate over each character 
 newString.foreach(println)
@@ -48,3 +48,60 @@ var lowerString = s.toLowerCase
 
 // ignore the cases and check if 2 strings are equal
 var equalityIgnoreCaseBoolean = dropString.equalsIgnoreCase(getString)
+
+// multiline string
+var newStringMultiline = """The quick brown
+fox jumps over
+the lazy dog"""
+println(newStringMultiline)
+
+// to add/remove margin
+var newStringExampleMargin = """ THE QUICK BROWN
+|FOX JUMPS OVER
+|THE LAZY DOG.""".stripMargin
+
+var anotherStringExampleMargin = """ THE QUICK BROWN
+#FOX JUMPS OVER
+#THE LAZY DOG.""".stripMargin('#')
+
+// Replace strings
+var newStringReplaced =  newStringExampleMargin.replaceAll("\n"," ")
+
+// Scala differentiates between " and '
+
+// split string
+// Returns an array
+var newStringSplit = newString.split(" ")
+
+// split a csv format
+var newStringSplitCSV = "eggs, basket".split(" ")
+
+// trim extra white spaces
+var newStringSplitCSVTrim = newStringSplitCSV.map(_.trim)
+
+// split a string based on the regular expressions
+var newStringSplitRegex = newString.split("\\s+")
+
+// substitue variables in string
+
+var newAgeInt = 21
+var newNameString = "Freddy"
+var newWeightDouble = 200.00
+// note the s in the beginning of the string 
+println(s"$newNameString is $newAgeInt old and weighs $newWeightDouble")
+
+
+// To perform operations in string 
+println(s"$newNameString is ${newAgeInt+1} old and weighs $newWeightDouble")
+
+// make a class
+case class Student(name: String, age: Int, weigh: Double)
+// add a new element to class
+var deepGupta = Student("Deepanshu", 21, 200.00)
+
+// perform operations in string
+println(s"${deepGupta.name} is ${deepGupta.age} old")
+var newStringVarInString = s"${deepGupta.name} is ${deepGupta.age} old"
+
+// printf style string formating
+var newStringVarInStringPrintfStyle = f"${deepGupta.name} is ${deepGupta.age} old and weighs ${deepGupta.weigh}%.2f"
